08cded11697d1f0eecff33edea01c0e171fe321d
Dendi <dendiaa@students.amikom.ac.id>
2024-04-15 22:09:07 +0700

    feat(middleware): for isauth page

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    const isAuthPage = req.nextUrl.pathname.startsWith("/sign-in");
+    if (isAuthPage) {
+      if (req.nextauth.token) {
+        return NextResponse.redirect(new URL("/mind", req.url));
+      }
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/sign-in", "/mind"],
+};
